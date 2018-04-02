5b5d0755655baf74ffd5add237f96392f830cba4
York_Luo <576716529@qq.com>
2018-04-02 16:02:51 +0800

    student-test

---
+++ student-test/src/main/java/org/student/controller/StudentController.java
@@ -0,0 +1,28 @@
+package org.student.controller;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.student.api.StudentApi;
+
+
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+	private static Logger logger = LoggerFactory.getLogger(StudentController.class);
+
+	@Resource
+	private StudentApi studentApi;
+	
+	@RequestMapping(path = "/listStudent", method = RequestMethod.GET)
+	public void listStudent(){
+		System.out.println("student-test:listStudent()");
+		logger.info("get students...");
+		logger.info("the data are " + studentApi.listStudents());
+	}
+}
